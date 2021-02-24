b12cdd97277965d7adf28df50c638eb782a3f908
Isa Hashim <isah@earthlink.net>
2021-02-23 16:25:46 -0800

    Add initial code for BLE setup - discover/connecting peripheral, discover service, characteristics.

--- MailboxAccess/MailboxAccess/MailboxManager.swift
+++ MailboxAccess/MailboxAccess/MailboxManager.swift
@@ -8,6 +8,8 @@
 import CoreBluetooth
 
 class MailboxManager: NSObject {
+    private let BRIGHTDROP_MBOX_NAME = "BD_MBOX"
+
     static let sharedInstance = MailboxManager()
 
     var centralMgr: CBCentralManager?
@@ -39,6 +41,7 @@ extension MailboxManager: CBCentralManagerDelegate {
         case .poweredOn:
             print("central.state is .poweredOn")
             // start scanning for peripherals
+            central.scanForPeripherals(withServices: nil, options: nil)
         case .poweredOff:
             print("central.state is .poweredOff")
         case .resetting:
@@ -51,6 +54,49 @@ extension MailboxManager: CBCentralManagerDelegate {
             print("central.state is .unsupported")
         @unknown default:
             print("central.state Returned unknown state")
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        guard let name = peripheral.name else {
+            return
+        }
+
+        if !name.contains(BRIGHTDROP_MBOX_NAME) {
+            return
+        }
+
+        print("Found device: " + peripheral.description)
+
+        mailboxPeripheral = peripheral
+        peripheral.delegate = self
+
+        centralMgr?.stopScan()
+        centralMgr?.connect(peripheral, options: nil)
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected: " + peripheral.description)
+        mailboxPeripheral?.discoverServices(nil)
+    }
+}
+
+extension MailboxManager: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+
+        for service in services {
+//            print(service)
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+
+        print(service)
+        for characteristic in characteristics {
+            print(" " + characteristic.description)
         }
     }
 }
